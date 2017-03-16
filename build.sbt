20dffabf3d2bcc1c22edc90c1ef4ad0e1d4c0a73
Christopher Blackwell <cwblackwell@gmail.com>
2017-03-16 11:31:38 -0400

    Pushing what I have.

--- build.sbt
+++ build.sbt
@@ -14,9 +14,9 @@ libraryDependencies ++= Seq(
   "org.scala-js" %%% "scalajs-dom" % "0.9.1",
   "com.lihaoyi" %%% "scalatags" % "0.6.3",
   "com.lihaoyi" %%% "scalarx" % "0.2.8",
-  "edu.holycross.shot" %%% "orca" % "3.0.0",
   "edu.holycross.shot.cite" %%% "xcite" % "2.2.1",
   "edu.holycross.shot" %%% "ohco2" % "6.12.0",
+  "edu.holycross.shot" %%% "orca" % "3.0.0",
   "edu.holycross.shot" %% "citeenv" % "1.1.0",
   "com.thoughtworks.binding" %%% "dom" % "latest.version"
 )
--- src/main/scala/citeappscalajs/CiteMainController.scala
+++ src/main/scala/citeappscalajs/CiteMainController.scala
@@ -55,23 +55,26 @@ object CiteMainController {
 		val reader = new org.scalajs.dom.raw.FileReader()
 		reader.readAsText(e.target.asInstanceOf[org.scalajs.dom.raw.HTMLInputElement].files(0))
 		reader.onload = (e: Event) => {
-//			val contents = reader.result.asInstanceOf[String]
-val contents:String = """#!ctsdata
-urn:cts:greekLit:tlg5026.msA.hmt:1.4.lemma#<div xmlns="http://www.tei-c.org/ns/1.0" n="lemma"> <p> θεά</p></div>
-urn:cts:greekLit:tlg5026.msA.hmt:1.4.comment#<div xmlns="http://www.tei-c.org/ns/1.0" n="comment"> <p> οὕτως εἴωθε τὴν <persName n="urn:cite:hmt:pers.pers6"> Μοῦσαν</persName> καλεῖν· ἀμέλει καὶ ἐν <title> Ὀδυσσεία</title> · <cit> <q> ἄνδρα μοι ἔννεπε <persName n="urn:cite:hmt:pers.pers6"> Μοῦσα</persName> <ref type="urn">
-#!ctscatalog
-urn#citationScheme#groupName#workTitle#versionLabel#exemplarLabel#online
-urn:cts:greekLit:tlg5026.msA.hmt:#book/scholion/part#Scholia to the Iliad#Main scholia of the Venetus A#HMT project edition##true
-"""
+			//			val contents = reader.result.asInstanceOf[String]
+			val tinyTexts = """#!ctsdata
+			urn:cts:greekLit:tlg5026.msA.hmt:1.4.lemma#<div xmlns="http://www.tei-c.org/ns/1.0" n="lemma"> <p> θεά</p></div>
+			urn:cts:greekLit:tlg5026.msA.hmt:1.4.comment#<div xmlns="http://www.tei-c.org/ns/1.0" n="comment"> <p> οὕτως εἴωθε τὴν <persName n="urn:cite:hmt:pers.pers6"> Μοῦσαν</persName> καλεῖν· ἀμέλει καὶ ἐν <title> Ὀδυσσεία</title> · <cit> <q> ἄνδρα μοι ἔννεπε <persName n="urn:cite:hmt:pers.pers6"> Μοῦσα</persName> <ref type="urn">
+			#!ctscatalog
+			urn#citationScheme#groupName#workTitle#versionLabel#exemplarLabel#online
+			urn:cts:greekLit:tlg5026.msA.hmt:#book/scholion/part#Scholia to the Iliad#Main scholia of the Venetus A#HMT project edition##true
+			"""
 
 			/* CRASHER BELOW!! */
+			val testUrn = CtsUrn("urn:cts:greekLit:auth.work:1.1")
+			println(testUrn.toString)
+			println(tinyTexts)
 
-			val cer = CiteExchangeReader(contents,"#")
-			val tr = cer.textRepository
-			println(s"Texts: ${tr.catalog.size}")
-			println(s"Texts: ${tr.corpus.size}")
+			val reader = CiteExchangeReader(tinyTexts,"#")
+			//val textRepo = reader.textRepository
+			//println(s"Texts: ${tr.catalog.size}")
+			//println(s"Texts: ${tr.corpus.size}")
 
-			CiteMainController.updateUserMessage(s"Loaded file of size ${contents.size}",0)
+			//CiteMainController.updateUserMessage(s"Loaded file of size ${contents.size}",0)
 		}
 	}
 
