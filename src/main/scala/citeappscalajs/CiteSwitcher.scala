06ea5728d17b7a0cd19feccc14294c55d6b1ec58
Christopher Blackwell <cwblackwell@gmail.com>
2017-05-25 09:38:21 -0400

    Adding class for handling communication between services.

---
+++ src/main/scala/citeappscalajs/CiteSwitcher.scala
@@ -0,0 +1,30 @@
+package citeapp
+
+import com.thoughtworks.binding.{Binding, dom}
+import com.thoughtworks.binding.Binding.{BindingSeq, Var, Vars}
+import scala.scalajs.js
+import scala.scalajs.js._
+import js.annotation._
+import collection.mutable
+import collection.mutable._
+import scala.scalajs.js.Dynamic.{ global => g }
+import org.scalajs.dom._
+import org.scalajs.dom.ext._
+import org.scalajs.dom.raw._
+import edu.holycross.shot.cite._
+import edu.holycross.shot.ohco2._
+import edu.holycross.shot.citeobj._
+
+import scala.scalajs.js.annotation.JSExport
+
+@JSExport
+object CiteSwitcher {
+
+
+	def switch(s:String):Unit = {
+			g.console.log(s"Will handle ${s}.")
+	}
+
+
+
+}
